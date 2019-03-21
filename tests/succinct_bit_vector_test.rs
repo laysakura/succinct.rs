4c37f81f9d9ecf124c17266f5094eb7f7736f4aa
Sho Nakatani <lay.sakura@gmail.com>
2019-03-21 14:51:53 +0900

    put integrated tests (#6)
    * put integrated tests
    
    * CI for only master branch

---
+++ tests/succinct_bit_vector_test.rs
@@ -0,0 +1,39 @@
+extern crate rust_succinct;
+
+use rust_succinct::succinct::bit_vector::{BitVectorBuilder, BitVectorString};
+
+#[test]
+fn build_from_length() {
+    let bv = BitVectorBuilder::from_length(2).build();
+    assert_eq!(bv.access(0), false);
+    assert_eq!(bv.access(1), false);
+}
+
+#[test]
+fn build_from_length_and_set_bit() {
+    let bv = BitVectorBuilder::from_length(2)
+        .set_bit(0)
+        .set_bit(1)
+        .set_bit(0)
+        .build();
+    assert_eq!(bv.access(0), true);
+    assert_eq!(bv.access(1), true);
+}
+
+#[test]
+fn build_from_str() {
+    let bv = BitVectorBuilder::from_str(BitVectorString {s: String::from("01")}).build();
+    assert_eq!(bv.access(0), false);
+    assert_eq!(bv.access(1), true);
+}
+
+#[test]
+fn build_from_str_and_set_bit() {
+    let bv = BitVectorBuilder::from_str(BitVectorString {s: String::from("00")})
+        .set_bit(0)
+        .set_bit(1)
+        .set_bit(0)
+        .build();
+    assert_eq!(bv.access(0), true);
+    assert_eq!(bv.access(1), true);
+}
