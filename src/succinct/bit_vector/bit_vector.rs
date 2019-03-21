4d8d0ff332df530bb52f8d2446aa3819bd6a5e52
Sho Nakatani <lay.sakura@gmail.com>
2019-03-21 13:34:12 +0900

    split files (#4)

---
+++ src/succinct/bit_vector/bit_vector.rs
@@ -0,0 +1,80 @@
+use super::BitVector;
+
+impl BitVector {
+    pub fn access(&self, i: usize) -> bool { self.rbv.access(i) }
+
+    pub fn rank(&self, i: usize) -> usize {
+        // TODO O(1) impl
+        (0.. (i + 1)).fold(0, |sum, j|
+            sum + if self.access(j) { 1 } else { 0 }
+        )
+    }
+}
+
+#[cfg(test)]
+mod access_success_tests {
+    // well-tested in bit_vector_builder::{builder_from_length_success_tests, builder_from_str_success_tests}
+}
+
+#[cfg(test)]
+mod access_failure_tests {
+    use super::super::BitVectorBuilder;
+
+    #[test]
+    #[should_panic]
+    fn over_upper_bound() {
+        let bv = BitVectorBuilder::from_length(2).build();
+        let _ = bv.access(2);
+    }
+}
+
+#[cfg(test)]
+mod rank_success_tests {
+    use super::super::BitVectorBuilder;
+
+    macro_rules! parameterized_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (in_bv_str, in_i, expected_rank) = $value;
+                assert_eq!(BitVectorBuilder::from_str(in_bv_str).build().rank(in_i), expected_rank);
+            }
+        )*
+        }
+    }
+
+    parameterized_tests! {
+        rank1_1: ("0", 0, 0),
+
+        rank2_1: ("00", 0, 0),
+        rank2_2: ("00", 1, 0),
+
+        rank3_1: ("01", 0, 0),
+        rank3_2: ("01", 1, 1),
+
+        rank4_1: ("10", 0, 1),
+        rank4_2: ("10", 1, 1),
+
+        rank5_1: ("11", 0, 1),
+        rank5_2: ("11", 1, 2),
+
+        rank6_1: ("10010", 0, 1),
+        rank6_2: ("10010", 1, 1),
+        rank6_3: ("10010", 2, 1),
+        rank6_4: ("10010", 3, 2),
+        rank6_5: ("10010", 4, 2),
+    }
+}
+
+#[cfg(test)]
+mod rank_failure_tests {
+    use super::super::BitVectorBuilder;
+
+    #[test]
+    #[should_panic]
+    fn rank_over_upper_bound() {
+        let bv = BitVectorBuilder::from_length(2).build();
+        let _ = bv.rank(2);
+    }
+}
---
+++ src/succinct/bit_vector/bit_vector_builder.rs
@@ -0,0 +1,321 @@
+use std::collections::HashSet;
+use super::{BitVector, BitVectorBuilder, BitVectorSeed};
+use crate::succinct::internal_data_structure::raw_bit_vector::RawBitVector;
+
+impl super::BitVectorBuilder {
+    pub fn from_length(length: usize) -> BitVectorBuilder {
+        BitVectorBuilder { seed: BitVectorSeed::Length(length), bits_set: HashSet::new() }
+    }
+
+    pub fn from_str(bit_vector_str: &str) -> BitVectorBuilder {
+        BitVectorBuilder { seed: BitVectorSeed::Str(String::from(bit_vector_str)), bits_set: HashSet::new() }
+    }
+
+    pub fn set_bit(&mut self, i: usize) -> &mut BitVectorBuilder {
+        self.bits_set.insert(i);
+        self
+    }
+
+    pub fn build(&self) -> BitVector {
+        let mut rbv = match &self.seed {
+            BitVectorSeed::Length(n) => RawBitVector::from_length(*n),
+            BitVectorSeed::Str(s) => RawBitVector::from_str(s),
+        };
+        for bit in &self.bits_set { rbv.set_bit(*bit) }
+        BitVector { rbv }
+    }
+}
+
+#[cfg(test)]
+mod builder_from_length_success_tests {
+    use super::BitVectorBuilder;
+
+    struct IndexBitPair(usize, bool);
+
+    macro_rules! parameterized_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (in_length, index_bit_pairs) = $value;
+                let bv = BitVectorBuilder::from_length(in_length).build();
+                for IndexBitPair(i, bit) in index_bit_pairs {
+                    assert_eq!(bv.access(i), bit);
+                }
+            }
+        )*
+        }
+    }
+
+    parameterized_tests! {
+        t1: (1, vec!(
+                     IndexBitPair(0, false),
+                )),
+        t2: (2, vec!(
+                     IndexBitPair(0, false),
+                     IndexBitPair(1, false),
+                )),
+        t8: (8, vec!(
+                     IndexBitPair(0, false),
+                     IndexBitPair(1, false),
+                     IndexBitPair(2, false),
+                     IndexBitPair(3, false),
+                     IndexBitPair(4, false),
+                     IndexBitPair(5, false),
+                     IndexBitPair(6, false),
+                     IndexBitPair(7, false),
+                )),
+        t9: (9, vec!(
+                     IndexBitPair(0, false),
+                     IndexBitPair(1, false),
+                     IndexBitPair(2, false),
+                     IndexBitPair(3, false),
+                     IndexBitPair(4, false),
+                     IndexBitPair(5, false),
+                     IndexBitPair(6, false),
+                     IndexBitPair(7, false),
+                     IndexBitPair(8, false),
+                )),
+    }
+}
+
+#[cfg(test)]
+mod builder_from_length_failure_tests {
+    use super::BitVectorBuilder;
+
+    #[test]
+    #[should_panic]
+    fn empty() {
+        let _ = BitVectorBuilder::from_length(0).build();
+    }
+}
+
+#[cfg(test)]
+mod builder_from_str_success_tests {
+    use super::BitVectorBuilder;
+
+    struct IndexBitPair(usize, bool);
+
+    macro_rules! parameterized_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (in_s, index_bit_pairs) = $value;
+                let bv = BitVectorBuilder::from_str(in_s).build();
+                for IndexBitPair(i, bit) in index_bit_pairs {
+                    assert_eq!(bv.access(i), bit);
+                }
+            }
+        )*
+        }
+    }
+
+    parameterized_tests! {
+        t1_1: ("0", vec!(
+                         IndexBitPair(0, false),
+                    )),
+        t1_2: ("1", vec!(
+                         IndexBitPair(0, true),
+                    )),
+
+        t2_1: ("00", vec!(
+                          IndexBitPair(0, false),
+                          IndexBitPair(1, false),
+                     )),
+        t2_2: ("01", vec!(
+                          IndexBitPair(0, false),
+                          IndexBitPair(1, true),
+                     )),
+        t2_3: ("10", vec!(
+                          IndexBitPair(0, true),
+                          IndexBitPair(1, false),
+                     )),
+        t2_4: ("11", vec!(
+                          IndexBitPair(0, true),
+                          IndexBitPair(1, true),
+                     )),
+
+        t8_1: ("00000000", vec!(
+                                IndexBitPair(0, false),
+                                IndexBitPair(1, false),
+                                IndexBitPair(2, false),
+                                IndexBitPair(3, false),
+                                IndexBitPair(4, false),
+                                IndexBitPair(5, false),
+                                IndexBitPair(6, false),
+                                IndexBitPair(7, false),
+                           )),
+        t8_2: ("11111111", vec!(
+                                IndexBitPair(0, true),
+                                IndexBitPair(1, true),
+                                IndexBitPair(2, true),
+                                IndexBitPair(3, true),
+                                IndexBitPair(4, true),
+                                IndexBitPair(5, true),
+                                IndexBitPair(6, true),
+                                IndexBitPair(7, true),
+                           )),
+        t8_3: ("01010101", vec!(
+                                IndexBitPair(0, false),
+                                IndexBitPair(1, true),
+                                IndexBitPair(2, false),
+                                IndexBitPair(3, true),
+                                IndexBitPair(4, false),
+                                IndexBitPair(5, true),
+                                IndexBitPair(6, false),
+                                IndexBitPair(7, true),
+                           )),
+
+        t9_1: ("000000000", vec!(
+                                 IndexBitPair(0, false),
+                                 IndexBitPair(1, false),
+                                 IndexBitPair(2, false),
+                                 IndexBitPair(3, false),
+                                 IndexBitPair(4, false),
+                                 IndexBitPair(5, false),
+                                 IndexBitPair(6, false),
+                                 IndexBitPair(7, false),
+                                 IndexBitPair(8, false),
+                            )),
+        t9_2: ("111111111", vec!(
+                                 IndexBitPair(0, true),
+                                 IndexBitPair(1, true),
+                                 IndexBitPair(2, true),
+                                 IndexBitPair(3, true),
+                                 IndexBitPair(4, true),
+                                 IndexBitPair(5, true),
+                                 IndexBitPair(6, true),
+                                 IndexBitPair(7, true),
+                                 IndexBitPair(8, true),
+                            )),
+        t9_3: ("101010101", vec!(
+                                 IndexBitPair(0, true),
+                                 IndexBitPair(1, false),
+                                 IndexBitPair(2, true),
+                                 IndexBitPair(3, false),
+                                 IndexBitPair(4, true),
+                                 IndexBitPair(5, false),
+                                 IndexBitPair(6, true),
+                                 IndexBitPair(7, false),
+                                 IndexBitPair(8, true),
+                            )),
+    }
+}
+
+#[cfg(test)]
+mod builder_from_str_failure_tests {
+    use super::BitVectorBuilder;
+
+    #[test]
+    #[should_panic]
+    fn empty() {
+        let _ = BitVectorBuilder::from_str("").build();
+    }
+
+    // well-tested in BitVectorString
+}
+
+#[cfg(test)]
+mod set_bit_success_tests {
+    use super::BitVectorBuilder;
+
+    struct IndexBitPair(usize, bool);
+
+    macro_rules! parameterized_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (in_s, bits_to_set, index_bit_pairs) = $value;
+                let mut builder = BitVectorBuilder::from_str(in_s);
+
+                for i in bits_to_set { builder.set_bit(i); }
+                let bv = builder.build();
+
+                for IndexBitPair(i, bit) in index_bit_pairs {
+                    assert_eq!(bv.access(i), bit);
+                }
+            }
+        )*
+        }
+    }
+
+    parameterized_tests! {
+        t1_1: ("0", vec!(),
+               vec!(
+                    IndexBitPair(0, false),
+                   )),
+        t1_2: ("0", vec!(0),
+               vec!(
+                    IndexBitPair(0, true),
+                   )),
+        t1_3: ("0", vec!(0, 0),
+               vec!(
+                    IndexBitPair(0, true),
+                   )),
+        t1_4: ("1", vec!(0),
+               vec!(
+                    IndexBitPair(0, true),
+                   )),
+
+        t8_1: ("00000000", vec!(),
+               vec!(
+                    IndexBitPair(0, false),
+                    IndexBitPair(1, false),
+                    IndexBitPair(2, false),
+                    IndexBitPair(3, false),
+                    IndexBitPair(4, false),
+                    IndexBitPair(5, false),
+                    IndexBitPair(6, false),
+                    IndexBitPair(7, false),
+                   )),
+        t8_2: ("00000000", vec!(0, 2, 4, 6),
+               vec!(
+                    IndexBitPair(0, true),
+                    IndexBitPair(1, false),
+                    IndexBitPair(2, true),
+                    IndexBitPair(3, false),
+                    IndexBitPair(4, true),
+                    IndexBitPair(5, false),
+                    IndexBitPair(6, true),
+                    IndexBitPair(7, false),
+                   )),
+
+        t9_1: ("000000000", vec!(),
+               vec!(
+                    IndexBitPair(0, false),
+                    IndexBitPair(1, false),
+                    IndexBitPair(2, false),
+                    IndexBitPair(3, false),
+                    IndexBitPair(4, false),
+                    IndexBitPair(5, false),
+                    IndexBitPair(6, false),
+                    IndexBitPair(7, false),
+                    IndexBitPair(8, false),
+                   )),
+        t9_2: ("000000000", vec!(0, 2, 4, 6, 8),
+               vec!(
+                    IndexBitPair(0, true),
+                    IndexBitPair(1, false),
+                    IndexBitPair(2, true),
+                    IndexBitPair(3, false),
+                    IndexBitPair(4, true),
+                    IndexBitPair(5, false),
+                    IndexBitPair(6, true),
+                    IndexBitPair(7, false),
+                    IndexBitPair(8, true),
+                   )),
+    }
+}
+
+#[cfg(test)]
+mod builder_set_bit_failure_tests {
+    use super::BitVectorBuilder;
+
+    #[test]
+    #[should_panic]
+    fn set_bit_over_upper_bound() {
+        let _ = BitVectorBuilder::from_length(2).set_bit(2).build();
+    }
+}
