0f32cf51373385ec8c6f8b159f783aa3a02ed132
Sho Nakatani <lay.sakura@gmail.com>
2019-04-08 06:25:41 +0900

    child_to_parent() - implementation and unit tests

--- src/louds/louds.rs
+++ src/louds/louds.rs
@@ -16,25 +16,36 @@ impl Louds {
     /// # Panics
     /// `index` does not point to any node in this LOUDS.
     pub fn index_to_node_num(&self, index: LoudsIndex) -> LoudsNodeNum {
-        assert_eq!(
-            self.lbs.access(index.value()),
-            true,
-            "LBS[index={:?}] must be '1'",
-            index,
-        );
+        self.validate_index(&index);
 
         let node_num = self.lbs.rank(index.value());
         LoudsNodeNum::new(node_num)
     }
 
     /// # Panics
-    /// `index` does not point to any node in this LOUDS.
+    /// - `index` does not point to any node in this LOUDS.
+    /// - `index == 0`: (node#1 is root and doesn't have parent)
     pub fn child_to_parent(&self, index: LoudsIndex) -> LoudsNodeNum {
-        LoudsNodeNum::new(1)
+        self.validate_index(&index);
+        assert!(index.value != 0, "node#1 is root and doesn't have parent");
+
+        let parent_node_num = self.lbs.rank0(index.value());
+        LoudsNodeNum::new(parent_node_num)
     }
 
     pub fn parent_to_children(&self, node_num: LoudsNodeNum) -> Vec<LoudsIndex> {
         vec![]
+    }
+
+    /// # Panics
+    /// `index` does not point to any node in this LOUDS.
+    fn validate_index(&self, index: &LoudsIndex) {
+        assert_eq!(
+            self.lbs.access(index.value()),
+            true,
+            "LBS[index={:?}] must be '1'",
+            index,
+        );
     }
 }
 
@@ -153,7 +164,7 @@ mod index_to_node_num_success_tests {
 mod index_to_node_num_failure_tests {
     use crate::{BitString, LoudsBuilder, LoudsIndex};
 
-    macro_rules! parameterized_node_not_found_tests {
+    macro_rules! parameterized_index_not_point_to_node_tests {
         ($($name:ident: $value:expr,)*) => {
         $(
             #[test]
@@ -168,7 +179,7 @@ mod index_to_node_num_failure_tests {
         }
     }
 
-    parameterized_node_not_found_tests! {
+    parameterized_index_not_point_to_node_tests! {
         t1_1: ("10_0", 1),
         t1_2: ("10_0", 3),
 
@@ -191,5 +202,106 @@ mod index_to_node_num_failure_tests {
         t3_12: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 22),
         t3_13: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 23),
         t3_14: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 24),
+    }
+}
+
+#[cfg(test)]
+mod child_to_parent_success_tests {
+    use crate::{BitString, LoudsBuilder, LoudsIndex, LoudsNodeNum};
+
+    macro_rules! parameterized_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (in_s, index, expected_parent) = $value;
+                let bs = BitString::new(in_s);
+                let louds = LoudsBuilder::from_bit_string(bs).build();
+                let parent = louds.child_to_parent(LoudsIndex::new(index));
+                assert_eq!(parent, LoudsNodeNum::new(expected_parent));
+            }
+        )*
+        }
+    }
+
+    parameterized_tests! {
+        t2_1: ("10_10_0", 2, 1),
+
+        t3_1: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 2, 1),
+        t3_2: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 3, 1),
+        t3_3: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 4, 1),
+        t3_4: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 6, 2),
+        t3_5: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 9, 4),
+        t3_6: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 10, 4),
+        t3_7: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 11, 4),
+        t3_8: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 15, 7),
+        t3_9: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 17, 8),
+        t3_10: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 18, 8),
+    }
+}
+
+#[cfg(test)]
+mod child_to_parent_failure_tests {
+    use crate::{BitString, LoudsBuilder, LoudsIndex};
+
+    macro_rules! parameterized_index_not_point_to_node_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            #[should_panic]
+            fn $name() {
+                let (in_s, index) = $value;
+                let bs = BitString::new(in_s);
+                let louds = LoudsBuilder::from_bit_string(bs).build();
+                let _ = louds.child_to_parent(LoudsIndex::new(index));
+            }
+        )*
+        }
+    }
+
+    parameterized_index_not_point_to_node_tests! {
+        t1_1: ("10_0", 1),
+        t1_2: ("10_0", 3),
+
+        t2_1: ("10_10_0", 1),
+        t2_2: ("10_10_0", 3),
+        t2_3: ("10_10_0", 4),
+        t2_4: ("10_10_0", 5),
+
+        t3_1: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 1),
+        t3_2: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 5),
+        t3_3: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 7),
+        t3_4: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 8),
+        t3_5: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 12),
+        t3_6: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 13),
+        t3_7: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 14),
+        t3_8: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 16),
+        t3_9: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 19),
+        t3_10: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 20),
+        t3_11: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 21),
+        t3_12: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 22),
+        t3_13: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 23),
+        t3_14: ("10_1110_10_0_1110_0_0_10_110_0_0_0", 24),
+    }
+
+    macro_rules! parameterized_root_not_have_parent_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            #[should_panic]
+            fn $name() {
+                let in_s = $value;
+                let bs = BitString::new(in_s);
+                let louds = LoudsBuilder::from_bit_string(bs).build();
+                let _ = louds.child_to_parent(LoudsIndex::new(0));
+            }
+        )*
+        }
+    }
+
+    parameterized_root_not_have_parent_tests! {
+        t1: "10_0",
+        t2: "10_10_0",
+        t3: "10_1110_10_0_1110_0_0_10_110_0_0_0",
     }
 }
